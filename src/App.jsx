36d14babac1a3d95e40c3a43659e5f9f60fb18f0
e1sndev <168357323+FaridM55@users.noreply.github.com>
2024-04-28 12:05:20 -0700

    configuring router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-const App = () => {};
+import { RouterProvider } from 'react-router';
+import Layout from './components/Layout';
+import router from './router';
+
+const App = () => {
+  return (
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  );
+};
 
 export default App;
