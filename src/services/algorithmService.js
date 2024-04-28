1eda8059bfe3987457f6c97214663f11c58f0fc7
e1sndev <168357323+FaridM55@users.noreply.github.com>
2024-04-28 11:51:44 -0700

    algorithmService

---
+++ src/services/algorithmService.js
@@ -0,0 +1,59 @@
+import api from './api';
+
+const algorithmService = api.injectEndpoints({
+  endpoints: (build) => ({
+    getAlgorithms: build.query({
+      query: () => ({
+        url: '/algorithms',
+      }),
+    }),
+
+    createaAlgorithm: build.mutation({
+      query: (body) => ({
+        url: '/algorithms/create',
+        method: 'POST',
+        body,
+      }),
+    }),
+
+    algorithmAddTestCase: build.mutation({
+      query: ({ id, body }) => ({
+        url: `/algorithms/{$id}/addTestCase`,
+        method: 'POST',
+        body,
+      }),
+    }),
+
+    algorithmSubmit: build.mutation({
+      query: ({ id, body }) => ({
+        url: `/algorithms/${id}/submit`,
+        method: 'POST',
+        body,
+      }),
+    }),
+
+    deleteAlgorithm: build.mutation({
+      query: (id) => ({
+        url: `/algorithms/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+
+    getAlgorithm: build.query({
+      query: (id) => ({
+        url: `/algorithms/${id}`,
+      }),
+    }),
+
+    updateAlgorithm: build.mutation({
+      query: ({ id, body }) => ({
+        url: `/algorithms/${id}`,
+        method: 'PUT',
+        body,
+      }),
+    }),
+  }),
+  overrideExisting: true,
+});
+
+export const { useGetAlgorithmsQuery } = algorithmService;
