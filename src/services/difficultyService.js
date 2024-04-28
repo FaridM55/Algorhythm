e18c193f1c7e7bf7fadbc3c099a58b26e8fb10d8
e1sndev <168357323+FaridM55@users.noreply.github.com>
2024-04-28 11:10:16 -0700

    difficultyService

---
+++ src/services/difficultyService.js
@@ -0,0 +1,33 @@
+import api from './api';
+
+const difficultyService = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getDifficulties: builder.query({
+      query: () => ({
+        url: '/difficulty',
+        method: 'GET',
+      }),
+    }),
+
+    createDifficulty: builder.mutation({
+      query: (body) => ({
+        url: '/difficulty',
+        method: 'POST',
+        body,
+      }),
+    }),
+
+    getDifficulty: builder.query({
+      query: (name) => ({
+        url: `/difficulty/${name}`,
+        method: 'GET',
+      }),
+    }),
+  }),
+  overrideExisting: true,
+});
+
+export const { useGetDifficultiesQuery, useCreateDifficultyMutation, useGetDifficultyQuery } =
+  difficultyService;
+
+export default difficultyService;
